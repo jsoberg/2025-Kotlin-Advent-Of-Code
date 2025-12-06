e4f6778125ca72288b3a7a76bfc137103c31febc
Joshua Soberg <soberg.dev@pm.me>
2025-12-06 11:06:59 -0500

    Update to latest utils, and leave mavenLocal up for ease of testing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "2025-Kotlin-Advent-Of-Code"
 
 dependencyResolutionManagement {
     repositories {
+        mavenLocal()
         mavenCentral()
         maven {
             url = uri("https://jitpack.io")
